bd68d45c033de2aafb99ea860f120c26b611005f
Ammar Abdo <90091361+AmmarAbdoh@users.noreply.github.com>
2024-07-03 14:00:42 +0300

    Finished InfoQuestion

--- src/QuestionsComponents/InfoQuestion.jsx
+++ src/QuestionsComponents/InfoQuestion.jsx
@@ -1,138 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { Container, Form, Button } from "react-bootstrap";
-import GetVerseInfo from "../functions/GetVerseInfo"; // Adjust the import path as necessary
-
-const surahNames = [
-  "الفاتحة",
-  "البقرة",
-  "آل عمران",
-  "النساء",
-  "المائدة",
-  "الأنعام",
-  "الأعراف",
-  "الأنفال",
-  "التوبة",
-  "يونس",
-  "هود",
-  "يوسف",
-  "الرعد",
-  "إبراهيم",
-  "الحجر",
-  "النحل",
-  "الإسراء",
-  "الكهف",
-  "مريم",
-  "طه",
-  "الأنبياء",
-  "الحج",
-  "المؤمنون",
-  "النور",
-  "الفرقان",
-  "الشعراء",
-  "النمل",
-  "القصص",
-  "العنكبوت",
-  "الروم",
-  "لقمان",
-  "السجدة",
-  "الأحزاب",
-  "سبأ",
-  "فاطر",
-  "يس",
-  "الصافات",
-  "ص",
-  "الزمر",
-  "غافر",
-  "فصلت",
-  "الشورى",
-  "الزخرف",
-  "الدخان",
-  "الجاثية",
-  "الأحقاف",
-  "محمد",
-  "الفتح",
-  "الحجرات",
-  "ق",
-  "الذاريات",
-  "الطور",
-  "النجم",
-  "القمر",
-  "الرحمن",
-  "الواقعة",
-  "الحديد",
-  "المجادلة",
-  "الحشر",
-  "الممتحنة",
-  "الصف",
-  "الجمعة",
-  "المنافقون",
-  "التغابن",
-  "الطلاق",
-  "التحريم",
-  "الملك",
-  "القلم",
-  "الحاقة",
-  "المعارج",
-  "نوح",
-  "الجن",
-  "المزمل",
-  "المدثر",
-  "القيامة",
-  "الإنسان",
-  "المرسلات",
-  "النبأ",
-  "النازعات",
-  "عبس",
-  "التكوير",
-  "الإنفطار",
-  "المطففين",
-  "الإنشقاق",
-  "البروج",
-  "الطارق",
-  "الأعلى",
-  "الغاشية",
-  "الفجر",
-  "البلد",
-  "الشمس",
-  "الليل",
-  "الضحى",
-  "الشرح",
-  "التين",
-  "العلق",
-  "القدر",
-  "البينة",
-  "الزلزلة",
-  "العاديات",
-  "القارعة",
-  "التكاثر",
-  "العصر",
-  "الهمزة",
-  "الفيل",
-  "قريش",
-  "الماعون",
-  "الكوثر",
-  "الكافرون",
-  "النصر",
-  "المسد",
-  "الإخلاص",
-  "الفلق",
-  "الناس",
-];
+import { Container } from "react-bootstrap";
+import GetVerseInfo from "../functions/GetVerseInfo";
+import SearchForInfo from "./SearchForInfo"; // Adjust the import path as necessary
+import VerseInfo from "../components/VerseInfo";
 
 const InfoQuestion = ({ verse, onNextQuestion }) => {
   const [question, setQuestion] = useState(null);
-  const [answer, setAnswer] = useState("");
   const [isCorrect, setIsCorrect] = useState(null);
 
   useEffect(() => {
-    // Retrieve verse info
     const verseInfo = GetVerseInfo({ verse });
 
-    // Define possible questions
     const questions = [
       {
         key: "اسم السورة",
         value: verseInfo[0].value,
+        searchType: 1,
         question: (
           <>
             <div className="mb-5">ما هو اسم السورة لهذه الاية:</div>
@@ -143,6 +26,7 @@ const InfoQuestion = ({ verse, onNextQuestion }) => {
       {
         key: "رقم الاية",
         value: verseInfo[1].value,
+        searchType: 2,
         question: (
           <>
             <div className="mb-5">ما هو رقم هذه الاية:</div>
@@ -154,6 +38,7 @@ const InfoQuestion = ({ verse, onNextQuestion }) => {
       {
         key: "رقم الجزء",
         value: verseInfo[2].value,
+        searchType: 3,
         question: (
           <>
             <div className="mb-5">ما هو رقم الجزء للآية:</div>
@@ -165,6 +50,7 @@ const InfoQuestion = ({ verse, onNextQuestion }) => {
       {
         key: "رقم الحزب",
         value: verseInfo[3].value,
+        searchType: 4,
         question: (
           <>
             <div className="mb-5">ما هو رقم الحزب للآية:</div>
@@ -176,6 +62,7 @@ const InfoQuestion = ({ verse, onNextQuestion }) => {
       {
         key: "الصفحة",
         value: verseInfo[4].value,
+        searchType: 5,
         question: (
           <>
             <div className="mb-5">ما هو رقم الصفحة للآية:</div>
@@ -186,19 +73,21 @@ const InfoQuestion = ({ verse, onNextQuestion }) => {
       },
     ];
 
-    // Randomly select a question
     const randomQuestion =
       questions[Math.floor(Math.random() * questions.length)];
     setQuestion(randomQuestion);
   }, [verse]);
 
-  const handleAnswerChange = (event) => {
-    setAnswer(event.target.value);
+  const handleSetAnswer = (answer) => {
+    if (question) {
+      setIsCorrect(answer === String(question.value));
+    }
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setIsCorrect(answer === String(question.value));
+  const handleNextQuestion = () => {
+    setIsCorrect(null);
+    setQuestion(null);
+    onNextQuestion();
   };
 
   return (
@@ -206,19 +95,12 @@ const InfoQuestion = ({ verse, onNextQuestion }) => {
       {question && (
         <div>
           <h4>{question.question}</h4>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="answer">
-              <Form.Label>الإجابة</Form.Label>
-              <Form.Control
-                type="text"
-                value={answer}
-                onChange={handleAnswerChange}
-              />
-            </Form.Group>
-            <btn className="mt-3 mybtn" type="submit">
-              تحقق
-            </btn>
-          </Form>
+          {isCorrect === null && (
+            <SearchForInfo
+              setAnswer={handleSetAnswer}
+              searchType={question.searchType}
+            />
+          )}
         </div>
       )}
 
@@ -229,9 +111,12 @@ const InfoQuestion = ({ verse, onNextQuestion }) => {
           ) : (
             <h3 style={{ color: "red" }}>اجابة خاطئة.</h3>
           )}
-          <btn className="mt-3 mybtn" onClick={onNextQuestion}>
+
+          <VerseInfo verse={verse} />
+
+          <button className="mt-3 mybtn" onClick={handleNextQuestion}>
             السؤال التالي
-          </btn>
+          </button>
         </div>
       )}
     </Container>
---
+++ src/QuestionsComponents/SearchForInfo.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useContext } from "react";
+import { MyContext } from "../useContext";
+import "../style/SearchForVerse.css";
+const generateOptions = (type) => {
+  switch (type) {
+    case 1:
+      return surahNames;
+    case 2:
+      return Array.from({ length: 286 }, (_, i) => (i + 1).toString());
+    case 3:
+      return Array.from({ length: 30 }, (_, i) => (i + 1).toString());
+    case 4:
+      return Array.from({ length: 60 }, (_, i) => (i + 1).toString());
+    case 5:
+      return Array.from({ length: 604 }, (_, i) => (i + 1).toString());
+    default:
+      return [];
+  }
+};
+
+const SearchForInfo = ({ setAnswer, searchType }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null); // State to hold the selected option temporarily
+  const options = generateOptions(searchType);
+
+  const filteredOptions = options
+    .filter((option) => option.includes(searchTerm))
+    .sort((a, b) => a.length - b.length);
+
+  const handleSelectOption = (option) => {
+    setSelectedOption(option);
+    setShowDropdown(false);
+  };
+
+  const confirmSelection = () => {
+    if (selectedOption) {
+      setAnswer(selectedOption);
+      setSelectedOption(null);
+      setSearchTerm("");
+    }
+  };
+
+  return (
+    <div className="search-container mt-5">
+      <input
+        className="input-field"
+        type="text"
+        placeholder="ابحث..."
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+          setShowDropdown(e.target.value !== "");
+        }}
+        onFocus={() => setShowDropdown(searchTerm !== "")}
+      />
+      {showDropdown && (
+        <div className="dropdown">
+          {filteredOptions.slice(0, 4).map((option, index) => (
+            <div
+              key={index}
+              className="dropdown-item"
+              onClick={() => handleSelectOption(option)}
+            >
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
+      {selectedOption && (
+        <div className="selected-verse">
+          <h6 className="mb-3">الاجابة المختارة</h6>
+          <h5>{selectedOption}</h5>
+          <button className="btn btn-success mt-3" onClick={confirmSelection}>
+            تأكيد الاختيار
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchForInfo;
